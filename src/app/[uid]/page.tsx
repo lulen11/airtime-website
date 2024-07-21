84073242dba6704c73cd11fa40b2c4195ebb8aa4
Lara Salameh <hello@larasalameh.com>
2024-07-21 11:09:00 +1000

    putting module page back

--- src/app/[uid]/page.tsx
+++ src/app/[uid]/page.tsx
@@ -1,44 +1,44 @@
 // TODO: REMOVE THIS EVENTUALLY. KEEPING HERE FOR REFERENCE
 
-// import { Metadata } from "next";
-// import { notFound } from "next/navigation";
-// import { SliceZone } from "@prismicio/react";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { SliceZone } from "@prismicio/react";
 
-// import { createClient } from "@/prismicio";
-// import { components } from "@/slices";
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
 
-// type Params = { uid: string };
+type Params = { uid: string };
 
-// export default async function Page({ params }: { params: Params }) {
-//   const client = createClient();
-//   const page = await client
-//     .getByUID("player_card", params.uid)
-//     .catch(() => notFound());
+export default async function Page({ params }: { params: Params }) {
+  const client = createClient();
+  const page = await client
+    .getByUID("player_card", params.uid)
+    .catch(() => notFound());
 
-//   return <SliceZone slices={page.data.slices} components={components} />;
-// }
+  return <SliceZone slices={page.data.slices} components={components} />;
+}
 
-// export async function generateMetadata({
-//   params,
-// }: {
-//   params: Params;
-// }): Promise<Metadata> {
-//   const client = createClient();
-//   const page = await client
-//     .getByUID("player_card", params.uid)
-//     .catch(() => notFound());
+export async function generateMetadata({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const client = createClient();
+  const page = await client
+    .getByUID("player_card", params.uid)
+    .catch(() => notFound());
 
-//   return {
-//     title: page.data.meta_title,
-//     description: page.data.meta_description,
-//   };
-// }
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
+}
 
-// export async function generateStaticParams() {
-//   const client = createClient();
-//   const pages = await client.getAllByType("player_card");
+export async function generateStaticParams() {
+  const client = createClient();
+  const pages = await client.getAllByType("player_card");
 
-//   return pages.map((page) => {
-//     return { uid: page.uid };
-//   });
-// }
+  return pages.map((page) => {
+    return { uid: page.uid };
+  });
+}
