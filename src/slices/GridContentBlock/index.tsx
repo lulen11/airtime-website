72d6c93a0215e09223176fb319a30897f5614682
Lara Salameh <hello@larasalameh.com>
2025-01-23 15:32:25 +1100

    setting up prismic grid content

--- src/slices/GridContentBlock/index.tsx
+++ src/slices/GridContentBlock/index.tsx
@@ -1,5 +1,7 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText } from "@prismicio/react";
+import styles from "./GridContentBlock.module.scss";
 
 /**
  * Props for `GridContentBlock`.
@@ -15,9 +17,16 @@ const GridContentBlock = ({ slice }: GridContentBlockProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={styles.sectionGridContentBlock}
     >
-      Placeholder component for grid_content_block (variation: {slice.variation}
-      ) Slices
+      <div className={`${styles.wrapper} `}>
+        <h3>{slice.primary.title}</h3>
+        {slice.primary.grid_items?.map((item, index) => (
+          <div className={styles.grid} key={index}>
+            <PrismicRichText field={item.content} />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
